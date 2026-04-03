5d96220456246ac3446fb51ea84698197b7c5931
Redmax80 <mardin.olmir02945608@gmail.com>
2026-04-03 12:15:47 +0700

    Изменен файл TASK_C1

--- TASK_C1.cpp
+++ TASK_C1.cpp
@@ -1,107 +1,105 @@
 #include <iostream>
 #include <cmath>
-//wydwycywfdsvuvbeuvuebvybvewbfvuewbfvwefywfd
 using namespace std;
-//iiiiiiiiiiiiiiiiiiiiiiiiiii
+
 int main() {
     setlocale(LC_ALL, "rus");
     double a, b, c, d;
     double h, P, S, midLine;
+    bool valid = false;
 
-    cout << "Введите сторону a: ";
-    cin >> a;
-    while (cin.fail() || a <= 0) {
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Пожалуйста, введите число!: ";
-        }
-        else {
-            cout << "Некорректное число, введите новое число: ";
-        }
+    
+    while (!valid) {
+        
+        cout << "Введите сторону a: ";
         cin >> a;
-    }
+        while (cin.fail() || a <= 0) {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "Пожалуйста, введите число!: ";
+            } else {
+                cout << "Некорректное число, введите новое число: ";
+            }
+            cin >> a;
+        }
 
-    cout << "Введите сторону b: ";
-    cin >> b;
-    while (cin.fail() || b <= 0) {
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Пожалуйста, введите число!: ";
-        }
-        else {
-            cout << "Некорректное число, введите новое число: ";
-        }
+        cout << "Введите сторону b: ";
         cin >> b;
-    }
+        while (cin.fail() || b <= 0) {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "Пожалуйста, введите число!: ";
+            } else {
+                cout << "Некорректное число, введите новое число: ";
+            }
+            cin >> b;
+        }
 
-    cout << "Введите сторону c: ";
-    cin >> c;
-    while (cin.fail() || c <= 0) {
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Пожалуйста, введите число!: ";
-        }
-        else {
-            cout << "Некорректное число, введите новое число: ";
-        }
+        cout << "Введите сторону c: ";
         cin >> c;
-    }
+        while (cin.fail() || c <= 0) {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "Пожалуйста, введите число!: ";
+            } else {
+                cout << "Некорректное число, введите новое число: ";
+            }
+            cin >> c;
+        }
 
-    cout << "Введите сторону d: ";
-    cin >> d;
-    while (cin.fail() || d <= 0) {
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Пожалуйста, введите число!: ";
-        }
-        else {
-            cout << "Некорректное число, введите новое число: ";
-        }
+        cout << "Введите сторону d: ";
         cin >> d;
+        while (cin.fail() || d <= 0) {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "Пожалуйста, введите число!: ";
+            } else {
+                cout << "Некорректное число, введите новое число: ";
+            }
+            cin >> d;
+        }
+
+        
+        if (a == 0 && b == 0 && c == 0 && d == 0) {
+            cout << "ОШИБКА!!! СТОРОНЫ РАВНЫ 0" << endl;
+            valid = false;
+            continue;
+        }
+
+        valid = true;
+
+        if (a == b) {
+            cout << "ОШИБКА!!! ОСНОВАНИЯ НЕ МОГУТ БЫТЬ РАВНЫ!!!" << endl;
+            valid = false;
+        }
+
+        double diff = (b > a) ? (b - a) : (a - b);
+        if (diff >= c + d) {
+            cout << "РАЗНОСТЬ ОСНОВАНИЙ НЕ МЕНЬШЕ СУММЫ БОКОВЫХ СТОРОН. ОШИБКА!!!" << endl;
+            valid = false;
+        }
+
+        double cd_diff = (c > d) ? (c - d) : (d - c);
+        if (diff <= cd_diff) {
+            cout << "РАЗНОСТЬ ОСНОВАНИЙ НЕ БОЛЬШЕ РАЗНОСТИ БОКОВЫХ СТОРОН. ОШИБКА!!!" << endl;
+            valid = false;
+        }
+
+        if (!valid) {
+            cout << "Повторите ввод сторон.\n";
+        }
     }
 
+    
+    midLine = (a + b) / 2;
+    double p = (a + b + c + d) / 2;
+    S = ((a + b) / abs(b - a)) * sqrt((p - a) * (p - b) * (p - a - c) * (p - a - d));
 
-    if (a == 0 && b == 0 && c == 0 && d == 0) {
-        cout << "ОШИБКА!!! СТОРОНЫ РАВНЫ 0" << endl;
-    }
-
-
-    bool valid = true;
-
-    if (a == b) {
-        cout << "ОШИБКА!!! ОСНОВАНИЯ НЕ МОГУТ БЫТЬ РАВНЫ!!!" << endl;
-        valid = false;
-    }
-
-    double diff = (b > a) ? (b - a) : (a - b); // |b-a|
-    if (diff >= c + d) {
-        cout << "РАЗНОСТЬ ОСНОВАНИЙ НЕ МЕНЬШЕ СУММЫ БОКОВЫХ СТОРОН. ОШИБКА!!!" << endl;
-        valid = false;
-    }
-
-    double cd_diff = (c > d) ? (c - d) : (d - c); // |c-d|
-    if (diff <= cd_diff) {
-        cout << "РАЗНОСТЬ ОСНОВАНИЙ НЕ БОЛЬШЕ РАЗНОСТИ БОКОВЫХ СТОРОН. ОШИБКА!!!" << endl;
-        valid = false;
-    }
-
-    if (valid) {
-        // Вычисляем среднюю линию трапеции
-        midLine = (a + b) / 2;
-
-        // Вычисляем площадь (формула Герона для трапеции)
-        double p = (a + b + c + d) / 2;
-        S = ((a + b) / abs(b - a)) * sqrt((p - a) * (p - b) * (p - a - c) * (p - a - d));
-    }
-    else {
-        S = -1.0; // признак ошибки
-        midLine = -1.0;
-    }
-
+    
     int choice;
     do {
         cout << "\nЧто ищем?" << endl;
@@ -120,35 +118,24 @@ int main() {
         }
 
         switch (choice) {
-        case 1:
-            P = a + b + c + d;
-            cout << "Периметр равен = " << P << endl;
-            break;
-        case 2:
-            if (valid && S >= 0) {
+            case 1:
+                P = a + b + c + d;
+                cout << "Периметр равен = " << P << endl;
+                break;
+            case 2:
                 cout << "Площадь равна = " << S << endl;
-            }
-            else {
-                cout << "Площадь не может быть вычислена. Некорректные стороны." << endl;
-            }
-            break;
-        case 3:
-            if (valid && midLine >= 0) {
+                break;
+            case 3:
                 cout << "Средняя линия равна = " << midLine << endl;
-            }
-            else {
-                cout << "Средняя линия не может быть вычислена." << endl;
-            }
-            break;
-        case 0:
-            cout << "EXIT!!!" << endl;
-            break;
-        default:
-            cout << "Неверный выбор. Введите число от 0 до 3" << endl;
+                break;
+            case 0:
+                cout << "EXIT!!!" << endl;
+                break;
+            default:
+                cout << "Неверный выбор. Введите число от 0 до 3" << endl;
         }
     } while (choice != 0);
 
     return 0;
-
-
 }
+
